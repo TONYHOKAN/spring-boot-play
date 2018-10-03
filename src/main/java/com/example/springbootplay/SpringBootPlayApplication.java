dc18216ee42dbc86f5e19f36898b8d51f0a342c1
small_tony <hokan@hktv.com.hk>
2018-10-03 21:02:20 +0800

    set up mvn build by config profile

---
+++ src/main/java/com/example/springbootplay/SpringBootPlayApplication.java
@@ -0,0 +1,21 @@
+package com.example.springbootplay;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+
+@SpringBootApplication
+public class SpringBootPlayApplication
+{
+	@Autowired
+	private static Environment env;
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SpringBootPlayApplication.class, args);
+		String property = env.getProperty("profile");
+		System.out.println("property   " + property);
+	}
+}
