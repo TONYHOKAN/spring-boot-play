d3d086d845264c9d06dffe362f3a8a1a3865f181
small_tony <hokan@hktv.com.hk>
2018-10-06 16:27:37 +0800

    we may want our own controller exception handling, add example, else spring boot will use DefaultHandlerExceptionResolver, the message quit good as well

---
+++ src/main/java/com/example/springbootplay/webservicecontroller/GlobalExceptionHandler.java
@@ -0,0 +1,60 @@
+package com.example.springbootplay.webservicecontroller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+/**
+ * Created by Tony Ng on 6/10/2018.
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler
+{
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@ExceptionHandler(Exception.class)
+	@ResponseBody
+	ResponseEntity<CustomizeErrorResponse> handleException(Exception e)
+	{
+		logger.error(e.getMessage(), e);
+
+		CustomizeErrorResponse response = new CustomizeErrorResponse();
+		response.setErrorCode("500");
+		response.setMessage("something wrong");
+
+		ResponseEntity<CustomizeErrorResponse> customizeErrorResponseResponseEntity = new ResponseEntity<>(response,
+				HttpStatus.INTERNAL_SERVER_ERROR);
+		return customizeErrorResponseResponseEntity;
+	}
+
+	private class CustomizeErrorResponse
+	{
+		private String errorCode;
+		private String message;
+
+		public String getErrorCode()
+		{
+			return errorCode;
+		}
+
+		public void setErrorCode(String errorCode)
+		{
+			this.errorCode = errorCode;
+		}
+
+		public String getMessage()
+		{
+			return message;
+		}
+
+		public void setMessage(String message)
+		{
+			this.message = message;
+		}
+	}
+}
