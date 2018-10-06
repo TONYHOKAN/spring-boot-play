a4b4b24d282bee92095826643f2145fe5fa73792
small_tony <hokan@hktv.com.hk>
2018-10-06 19:31:35 +0800

    tmp

---
+++ src/test/java/com/example/springbootplay/controllertest/UserControllerTest.java
@@ -0,0 +1,8 @@
+package com.example.springbootplay.controllertest;
+
+/**
+ * Created by Tony Ng on 6/10/2018.
+ */
+public class UserControllerTest
+{
+}
---
+++ src/test/java/com/example/springbootplay/unittest/UserMapperUnitTest.java
@@ -0,0 +1,8 @@
+package com.example.springbootplay.unittest;
+
+/**
+ * Created by Tony Ng on 6/10/2018.
+ */
+public class UserMapperUnitTest
+{
+}
