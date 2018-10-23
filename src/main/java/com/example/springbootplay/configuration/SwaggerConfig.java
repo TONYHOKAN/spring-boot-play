f481a853b3163b456310f3ad1ae5ff3099d77c2a
small_tony <hokan@hktv.com.hk>
2018-10-23 09:44:45 +0800

    fix as controller package changed

--- src/main/java/com/example/springbootplay/configuration/SwaggerConfig.java
+++ src/main/java/com/example/springbootplay/configuration/SwaggerConfig.java
@@ -26,7 +26,7 @@ public class SwaggerConfig
 		return new Docket(DocumentationType.SWAGGER_2)
 				.apiInfo(apiInfo())
 				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.example.springbootplay.webservicecontroller"))
+				.apis(RequestHandlerSelectors.basePackage("com.example.springbootplay.controller.webservice"))
 				.paths(PathSelectors.any())
 				.build();
 	}
